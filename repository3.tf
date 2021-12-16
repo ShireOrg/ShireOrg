3b3cfde9357d7b411cec0428477e6b40f01e5887
Michael Shire <mike_shire@hotmail.com>
2021-12-16 17:11:32 -0500

    Delete repository3.tf

--- repository3.tf
+++
@@ -1,8 +0,0 @@
-resource "github_repository" "repository789" {
-  name        = "repository789"
-  description = "My awesome codebase"
-
-  visibility = "private"
-  auto_init  = true
-  has_issues = true
-}

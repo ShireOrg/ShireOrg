0083b93c4413c5c0e7be4b7c6034cd0c3bcea983
Michael Shire <mike_shire@hotmail.com>
2021-12-19 19:31:31 -0500

    Terraform formatting

--- repository1.tf
+++ repository1.tf
@@ -2,9 +2,9 @@ resource "github_repository" "repository345" {
   name        = "repository345"
   description = "My awesome codebase"
 
-  visibility = "private"
-  auto_init  = true
-  has_issues = true
-  has_wiki = true
+  visibility   = "private"
+  auto_init    = true
+  has_issues   = true
+  has_wiki     = true
   has_projects = true
 }

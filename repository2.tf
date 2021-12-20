5f50ead6072d35cb0f3fb3a5c6fd4cacc5b4effe
Michael Shire <mike_shire@hotmail.com>
2021-12-19 19:07:38 -0500

    Delete repository2.tf

--- repository2.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository" "repository234" {
-  name        = "repository234"
-  description = "My awesome codebase"
-
-  visibility = "private"
-  auto_init  = true
-  has_issues = true
-
-}
